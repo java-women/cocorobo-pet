46895ce8e7f334524c02e28b9a01d9b70b16b37a
erk5 <e.hirajiri@gmail.com>
2016-09-11 17:49:00 +0900

    [cocoroboController] Initial commit for Spring Boot project.

---
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/CocoroboPetApplication.java
@@ -0,0 +1,12 @@
+package javajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoroboPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocoroboPetApplication.class, args);
+	}
+}
