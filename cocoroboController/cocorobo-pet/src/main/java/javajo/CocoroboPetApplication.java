d098844291ae7969478953cc3f94e5fad8c09f18
erk5 <e.hirajiri@gmail.com>
2016-09-12 23:50:37 +0900

    [cocoroboController] Modify the gradle settings from jar to war.

--- cocoroboController/cocorobo-pet/src/main/java/javajo/CocoroboPetApplication.java
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/CocoroboPetApplication.java
@@ -2,11 +2,19 @@ package javajo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CocoroboPetApplication {
+public class CocoroboPetApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CocoroboPetApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CocoroboPetApplication.class);
+	}
+
 }
