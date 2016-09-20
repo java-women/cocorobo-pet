4303579695ca39a1fb04c0fad032220a90ade448
miho <omame1986@gmail.com>
2016-09-21 01:05:50 +0900

    ・撮影 ・送信処理 （送信処理はURLが適当なので落ちます）

--- androidApp/CocoroboPet/app/src/main/java/com/java_women/cocorobopet/MainActivity.java
+++ androidApp/CocoroboPet/app/src/main/java/com/java_women/cocorobopet/MainActivity.java
@@ -1,19 +1,25 @@
 package com.java_women.cocorobopet;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.os.Bundle;
 
-/**
- * menu画面.
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button photoButton = (Button)findViewById(R.id.photo);
+        photoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,PhotoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
 
         clickFeelButton();
     }
