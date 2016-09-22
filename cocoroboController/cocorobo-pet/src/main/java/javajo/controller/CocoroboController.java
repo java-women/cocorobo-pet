f81237ab2cbe2eac69e55a19df091f790801eabb
erk5 <e.hirajiri@gmail.com>
2016-09-22 18:51:55 +0900

    [CocoroboController] Add interface of speech.

--- cocoroboController/cocorobo-pet/src/main/java/javajo/controller/CocoroboController.java
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/controller/CocoroboController.java
@@ -1,15 +1,13 @@
-package javajo;
+package javajo.controller;
 
 import javajo.dto.FeelDTO;
+import javajo.dto.SpeechDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -57,6 +55,32 @@ public class CocoroboController {
         /* モックデータ作成終了 */
 
         return new ResponseEntity(feelDTO, HttpStatus.OK);
+    }
+
+    /**
+     * 指定されたCOCOROBOに指定した文字列で音声発話させる.
+     *
+     * @param cocorobo COCOROBOの識別子：例)toko
+     * @param message
+     * @return JSON形式の文字列(COCOROBO音声発話APIのレスポンス)：<pre>{@code
+     * {
+     *     "resultCode": 結果コード,
+     *     "data":レスポンスデータ詳細(子要素はnull),
+     *     "errorCode": エラーコード,
+     *     "errorMessage":エラーメッセージ
+     * }
+     * }</pre>
+     */
+    @PostMapping(value = "/speeches/{cocorobo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SpeechDTO> speech(@PathVariable String cocorobo, @RequestBody String message) {
+        log.info("REST request speech : {}, message : {}",cocorobo, message);
+
+        /* モックデータ作成開始 */
+        SpeechDTO speechDTO = new SpeechDTO();
+        speechDTO.setResultCode(0);
+        /* モックデータ作成終了 */
+
+        return new ResponseEntity(new SpeechDTO(), HttpStatus.OK);
     }
 
 }
