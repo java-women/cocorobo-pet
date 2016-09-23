7acdf67908af64c168b32d2e0dcab057aae9ad92
mppppp <motimotipotatomato@gmail.com>
2016-09-23 11:10:44 +0900

    返却されるJSONの形を追加しました

---
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/ApiKey.java
@@ -0,0 +1,14 @@
+package javajo;
+
+/**
+ * Created by ohashi on 2016/09/23.
+ */
+public class ApiKey {
+    private String apiKey = "";
+
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+}
---
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/dto/RequestVerify.java
@@ -0,0 +1,17 @@
+package javajo.dto;
+
+/**
+ * Created by ohashi on 2016/09/23.
+ */
+public class RequestVerify {
+    private String faceId1;
+    private String faceId2;
+
+    public void setFaceId1(String faceId1) {
+        this.faceId1 = faceId1;
+    }
+
+    public void setFaceId2(String faceId2) {
+        this.faceId2 = faceId2;
+    }
+}
