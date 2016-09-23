331888aeb1b12b83b95c2e1df465332acd786823
mppppp <motimotipotatomato@gmail.com>
2016-09-24 02:10:34 +0900

    レビューを元に修正を行いました

--- cocoroboController/cocorobo-pet/src/main/java/javajo/dto/RequestVerify.java
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/dto/RequestVerify.java
@@ -1,17 +1,19 @@
 package javajo.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * Created by ohashi on 2016/09/23.
  */
 public class RequestVerify {
     private String faceId1;
-    private String faceId2;
+    private MultipartFile photo;
 
     public void setFaceId1(String faceId1) {
         this.faceId1 = faceId1;
     }
 
-    public void setFaceId2(String faceId2) {
-        this.faceId2 = faceId2;
+    public void setPhoto(MultipartFile photo) {
+        this.photo = photo;
     }
 }
