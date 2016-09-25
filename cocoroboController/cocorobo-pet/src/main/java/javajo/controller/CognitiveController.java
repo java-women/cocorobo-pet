9f82ab0e671d23144a87aeaef6149bceb1a2cd04
maaya <huki.j37@gmail.com>
2016-09-25 15:09:09 +0900

    serviceじゃなくてhelperだった件

--- cocoroboController/cocorobo-pet/src/main/java/javajo/controller/CognitiveController.java
+++ cocoroboController/cocorobo-pet/src/main/java/javajo/controller/CognitiveController.java
@@ -3,6 +3,10 @@ package javajo.controller;
 import javajo.dto.CompareImageDTO;
 import javajo.dto.RegisterImgDTO;
 import javajo.enums.StatusEnum;
+import javajo.hepler.FileHelper;
+import javajo.model.detect.Detect;
+import javajo.hepler.CognitiveHelper;
+import javajo.hepler.RedisHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * MS Azure CognitiveAPIとやりとりをするAPI.
@@ -37,20 +43,44 @@ public class CognitiveController {
      */
     @PostMapping(value = "/registerImage", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity registerImage(@RequestParam("userId") String userId, @RequestParam("faceImage") MultipartFile faceImg) {
-        log.info("REST request registerImg : {}, faceImg : {}", userId, faceImg);
+        log.info("REST request registerImage : {}, faceImage : {}", userId, faceImg);
+
+        RegisterImgDTO results = new RegisterImgDTO();
+
+        //画像をローカルに保存
+        String uploadFile;
+        FileHelper fileHelper = new FileHelper();
+        try {
+            uploadFile = fileHelper.saveFile(faceImg);
+        } catch (IOException e) {
+            results.setResult(StatusEnum.NG.getName());
+            return new ResponseEntity<>(results, HttpStatus.BAD_REQUEST);
+        }
+
+        //受け取った写真データをDetectAPIに送付
+        CognitiveHelper cognitive = new CognitiveHelper();
+        Detect[] faceInfo = cognitive.makeFaceInfo(uploadFile);
+
+        //DetectAPIのデータをredisに登録
+        RedisHelper redis = new RedisHelper();
+        try{
+
+            redis.setKeyValue("user", );
+        }
+
+
+        //responce作成
 
         /* モックデータ作成開始 */
-        RegisterImgDTO registerImgDTO = new RegisterImgDTO();
-        registerImgDTO.setResult(StatusEnum.OK.getName());
-        registerImgDTO.setFaceId("aaaaaaaaaaaaaa");
-        /* モックデータ作成終了 */
+//        registerImgDTO.setResult(StatusEnum.OK.getName());
+//        registerImgDTO.setFaceId("aaaaaaaaaaaaaa");
+//        /* モックデータ作成終了 */
 
         return new ResponseEntity(registerImgDTO, HttpStatus.OK);
     }
 
     @PostMapping(value = "/compareImage", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CompareImageDTO> compareImage(@RequestParam("compareFile") MultipartFile multipartFile) {
-
         CompareImageDTO results = new CompareImageDTO();
 
         // ファイルが空の場合は異常終了
@@ -58,6 +88,15 @@ public class CognitiveController {
             results.setResult(StatusEnum.NG.getName());
             return new ResponseEntity<>(results, HttpStatus.BAD_REQUEST);
         }
+
+
+        //受け取った写真データをDetectAPIに送付
+
+        //Redisから教師データを取得
+
+        //varifyAPI
+
+        //判定結果返却
 
         results.setResult(StatusEnum.OK.getName());
 
