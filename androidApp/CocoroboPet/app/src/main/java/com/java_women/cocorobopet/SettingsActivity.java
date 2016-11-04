0a26fb083ea0d8222f84d1076d860aec8a15fc1b
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-11-04 23:50:59 +0900

    add settings page

---
+++ androidApp/CocoroboPet/app/src/main/java/com/java_women/cocorobopet/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.java_women.cocorobopet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 設定画面.
+ * Created by aya on 2016/11/04.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+}
